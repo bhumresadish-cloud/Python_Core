17daf6e3edd5feeb506d945b9e95792453548410
Sadish Bhumre <bhumresadish@gmail.com>
2026-03-30 15:33:15 +0530

    new code

--- controls.py
+++ controls.py
@@ -1,26 +1,38 @@
 
+#If else seatement 5 program
+
+#num=int(input("Enter a number:"))
+#if num%2==0:
+#   print("It is even num")
+
+#else:
+#   print("It is Odd")
+
 #elif statement 5 program
-mark=int(input("enter your marks for appricition:"))
-if mark==60
+#mark=int(input("Enter your marks for appricition:"))
+#if mark==60:
+#    print("Average marks")
 
+#elif mark==70:
+#    print("Good marks")
 
+#elif mark==80:
+#    print("Best marks")
 
+#else:
+#    print("Invalid marks")
 
+#Nested If 5 program
 
+num1=58
+if num1>0:     #57>0 true
+    print(" It is positive Number")
 
-
-
-
-
-
-
-# Nested If
-num1=56
-if num1>0: #57>0 true
-    print("It is positive Number")
-
-    if num1%2==0:   #57%2==0 false
-        print("yes its even Also")
+    if num1%2==0:     #57%2==0 false
+        print("Yes its Even number")
 
     else:
-        print
+        print(" Its odd Also")
+
+else:
+    print("It is negative that's why we cant check ")
--- datatypes.py
+++ datatypes.py
@@ -1,5 +1,12 @@
-a=[12,"hello",56.89,true]
+
+a=[12,"hello",56.89,True]     #list
 print(a)
 
-b=(12,"hello",)
-print
+b=(12,"hello",56.89,True)    #tuple
+print(b)
+
+c=56,78,34,'a'       #set
+print(c)
+
+d={1:111,2:222,3:333}  #dict
+print(d)
